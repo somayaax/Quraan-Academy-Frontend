f361429ba6b0dfcba3393a77ccdabfef943ce32d
hayah1999 <hayahhazem1999@gmail.com>
2023-07-02 21:34:05 +0300

    filter student recordedCourses by finished or inProgress

--- src/app/modules/student/components/recourded-courses/recourded-courses.component.ts
+++ src/app/modules/student/components/recourded-courses/recourded-courses.component.ts
@@ -1,47 +1,70 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, SimpleChanges } from "@angular/core";
 import { RecordedCourseService } from "../../services/recordedCourses/recorded-course.service";
 import { ToastrService } from "ngx-toastr";
 
 @Component({
-    selector: "app-recourded-courses",
-    templateUrl: "./recourded-courses.component.html",
-    styleUrls: ["./recourded-courses.component.css"],
+  selector: "app-recourded-courses",
+  templateUrl: "./recourded-courses.component.html",
+  styleUrls: ["./recourded-courses.component.css"],
 })
 export class RecourdedCoursesComponent implements OnInit {
-    recordedCourses: studentRecordedCoursesElement[] = [];
-    constructor(
-        private recordedCourse: RecordedCourseService,
-        private toastr: ToastrService
-    ) { }
+  recordedCourses: studentRecordedCoursesElement[] = [];
+  filteredCourses: any[] = [];
+  selectedCategory: any = "";
+  constructor(
+    private recordedCourse: RecordedCourseService,
+    private toastr: ToastrService
+  ) { }
 
-    getRecordedCourses(): void {
-        this.recordedCourse.getStudentRecordedCourses().subscribe({
-            next: (data) => {
-                this.recordedCourses = data;
-            },
-            error: (error: any) => {
-                let {
-                    error: { message },
-                } = error;
-                if (!message) message = error.error.error;
-                this.toastr.error(`${message}`, "Error");
-            },
-        });
+  getRecordedCourses(): void {
+    this.recordedCourse.getStudentRecordedCourses().subscribe({
+      next: (data) => {
+        this.recordedCourses = data;
+        this.filteredCourses = this.recordedCourses;
+      },
+      error: (error: any) => {
+        let {
+          error: { message },
+        } = error;
+        if (!message) message = error.error.error;
+        this.toastr.error(`${message}`, "Error");
+      },
+    });
+  }
+  ngOnInit(): void {
+    this.getRecordedCourses();
+    this.filterCourses();
+  }
+
+  onSelectCategory(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    if (target) {
+      const category = target.value;
+      this.selectedCategory = category;
+      this.filterCourses();
     }
-    ngOnInit(): void {
-        this.getRecordedCourses();
+  }
+
+  filterCourses(): void {
+    if (this.selectedCategory === 'finished') {
+      this.filteredCourses = this.recordedCourses.filter(course => course.progress.length === course.courseID.numberOfChapters);
+    } else if (this.selectedCategory === 'inProgress') {
+      this.filteredCourses = this.recordedCourses.filter(course => course.progress.length !== course.courseID.numberOfChapters);
+    } else {
+      this.filteredCourses = this.recordedCourses;
     }
+  }
 }
 
 export interface studentRecordedCoursesElement {
-    _id?: string;
-    studentId: string;
-    courseID: {
-        _id: string;
-        name: string;
-        price: number;
-        numberOfChapters: number;
-        category: string;
-    };
-    progress: string[];
+  _id?: string;
+  studentId: string;
+  courseID: {
+    _id: string;
+    name: string;
+    price: number;
+    numberOfChapters: number;
+    category: string;
+  };
+  progress: string[];
 }
